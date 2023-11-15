a83583d486463ad4aa3eb3a2121598e76be23ba9
ceedubbs <swong24@andover.edu>
2023-11-15 14:01:28 -0500

    big bang

---
+++ js/artifacts.js
@@ -0,0 +1,379 @@
+let artifacts = [
+  {
+    id: 1,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Buddhist%20Flag%20and%20Olcott.pdf",
+    title: "Buddhist Flag and Olcott",
+    reflections: [],
+  },
+  {
+    id: 2,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Olcott%20in%20Colombo%20Sri%20Lanka.pdf",
+    title: "Olcott in Colombo Sri Lanka",
+    reflections: [],
+  },
+  {
+    id: 3,
+    type: "video",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Ryo%20Imamura%20Segment%2017.mp4",
+    title: "Ryo Imamura Segment 17",
+    reflections: [],
+  },
+  {
+    id: 4,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/People%20leaving%20Buddhist%20church,%20winter,%20Manzanar%20Relocation%20Center,%20California.pdf",
+    title:
+      "People leaving Buddhist church, winter, Manzanar Relocation Center, California",
+    reflections: [],
+  },
+  {
+    id: 5,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Buddhists%20at%20the%20Santa%20Fe%20Internment%20Camp.pdf",
+    title: "Buddhists at the Santa Fe Internment Camp",
+    reflections: [],
+  },
+  {
+    id: 6,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Quotes%20from%20Ginsberg.pdf",
+    title: "Quotes from Ginsberg",
+    reflections: [],
+  },
+  {
+    id: 7,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Allen%20Ginsberg%20Meditation%20and%20Poetics.pdf",
+    title: "Allen Ginsberg Meditation and Poetics",
+    reflections: [],
+  },
+  {
+    id: 8,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Excerpt%20from%20Thoreau.pdf",
+    title: "Excerpt from Thoreau",
+    reflections: [],
+  },
+  {
+    id: 9,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Robert%20Thurman%20Photos.pdf",
+    title: "Robert Thurman Photos",
+    reflections: [],
+  },
+  {
+    id: 10,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Mexico%20City%20Blues%20113th%20Chorus.pdf",
+    title: "Mexico City Blues 113th Chorus",
+    reflections: [],
+  },
+  {
+    id: 11,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Thich%20Nhat%20Hanh%20and%20MLK.pdf",
+    title: "Thich Nhat Hanh and MLK",
+    reflections: [],
+  },
+  {
+    id: 12,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Ryo%20Imamura%20Bio.pdf",
+    title: "Ryo Imamura Bio",
+    reflections: [],
+  },
+  {
+    id: 13,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Ryo%20Imamura%20Letter%20to%20Tricycle.pdf",
+    title: "Ryo Imamura Letter to Tricycle",
+    reflections: [],
+  },
+  {
+    id: 14,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Buddhist%20service%20at%20Manzanar%20Relocation%20Center%20in%20California.pdf",
+    title: "Buddhist service at Manzanar Relocation Center in California",
+    reflections: [],
+  },
+  {
+    id: 15,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Allen%20Ginsberg%20Poetry%20and%20Meditation.pdf",
+    title: "Allen Ginsberg Poetry and Meditation",
+    reflections: [],
+  },
+  {
+    id: 16,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Excerpts%20from%20Just%20Culture.pdf",
+    title: "Excerpts from Just Culture",
+    reflections: [],
+  },
+  {
+    id: 17,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Why%20I'm%20Angry.pdf",
+    title: "Why I'm Angry",
+    reflections: [],
+  },
+  {
+    id: 18,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Destruction%20of%20Shinto%20Shrines%20in%20Hawaii.pdf",
+    title: "Destruction of Shinto Shrines in Hawaii",
+    reflections: [],
+  },
+  {
+    id: 19,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/The%20Canticle%20of%20Jack%20Kerouac.pdf",
+    title: "The Canticle of Jack Kerouac",
+    reflections: [],
+  },
+  {
+    id: 20,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Smokey%20The%20Bear%20Sutra.pdf",
+    title: "Smokey The Bear Sutra",
+    reflections: [],
+  },
+  {
+    id: 21,
+    type: "pdf",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/To%20Meditate.pdf",
+    title: "To Meditate",
+    reflections: [],
+  },
+  {
+    id: 22,
+    type: "video",
+    url: "https://petjubxyyhciqivlwinq.supabase.co/storage/v1/object/public/artifacts/Allen%20Ginsberg%20sings%20%20Do%20The%20Meditation%20Rock%20%201984.mp4",
+    title: "Allen Ginsberg sings Do The Meditation Rock 1984",
+    reflections: [],
+  },
+];
+
+let visitedArtifacts = new Set();
+let currentArtifact = null;
+let username = "";
+
+function saveArtifactsAndReflections() {
+  const artifactsData = {
+    visitedArtifacts: [...visitedArtifacts],
+    artifacts: artifacts, // Assuming 'artifacts' is an array of artifact objects
+  };
+  localStorage.setItem("artifactsData", JSON.stringify(artifactsData));
+}
+
+function loadArtifactsAndReflections() {
+  const savedData = JSON.parse(localStorage.getItem("artifactsData"));
+  if (savedData) {
+    visitedArtifacts = new Set(savedData.visitedArtifacts);
+    artifacts = savedData.artifacts;
+  }
+}
+
+// document.getElementById("totalArtifacts").textContent = artifacts.length;
+
+async function displayArtifact(artifact) {
+  console.log("displayArtifact function called");
+  console.log("Artifact:", artifact);
+  const viewer = document.getElementById("artifactViewer");
+  console.log("Iframe element:", viewer);
+  viewer.style.display = "block"; // Show the iframe
+  viewer.style.width = "80%";
+  viewer.style.height = "800px";
+
+  // Update the artifact title
+  const titleElement = document.querySelector(".rl-heading-style-h2-3");
+  if (titleElement) {
+    titleElement.textContent = artifact.title; // Update this line as per your artifact object property
+  }
+  currentArtifact = artifact;
+  artifactUrl = currentArtifact.url;
+
+  console.log(artifactUrl);
+  console.log(currentArtifact.type);
+
+  // Directly load the artifact URL into the iframe
+  viewer.src = artifactUrl;
+  visitedArtifacts.add(artifact.id);
+  saveArtifactsAndReflections();
+
+  // Add error handling for loading the artifact
+  viewer.onerror = function () {
+    console.log("Error loading artifact");
+  };
+}
+async function loadArtifact(artifactId) {
+  console.log("load artifact called");
+  // Convert artifactId to a number
+  const numericArtifactId = Number(artifactId);
+  const artifact = artifacts.find((a) => a.id === numericArtifactId);
+  if (artifact) {
+    await displayArtifact(artifact);
+  } else {
+    console.log("Artifact not found with ID:", numericArtifactId);
+  }
+}
+
+async function loadRandomArtifact() {
+  console.log("load random function called");
+  let remainingArtifacts = artifacts.filter((a) => !visitedArtifacts.has(a.id));
+  console.log(visitedArtifacts.size);
+  if (visitedArtifacts.size === 5) {
+    // Redirect to a specific page when all artifacts are visited
+    window.location.href = "summary-page.html";
+    return;
+  }
+  const randomIndex = Math.floor(Math.random() * remainingArtifacts.length);
+  await displayArtifact(remainingArtifacts[randomIndex]);
+}
+function handleContinue() {
+  // Save the reflection
+  const reflectionInput = document.getElementById("reflections");
+  if (currentArtifact) {
+    if (!currentArtifact.reflections) {
+      currentArtifact.reflections = [];
+    }
+    currentArtifact.reflections.push(reflectionInput.value);
+    saveArtifactsAndReflections();
+  }
+
+  // Clear the reflection input
+  reflectionInput.value = "";
+
+  // Load a new random artifact
+  loadRandomArtifact();
+}
+function populateArtifactLinks() {
+  const container = document.getElementById("artifactsContainer");
+  artifacts.forEach((artifact) => {
+    // Create the link element
+    const link = document.createElement("a");
+    link.href = `artifact.html?artifactId=${artifact.id}`; // Redirect to artifacts.html with query parameter
+    link.className = "w-inline-block";
+    link.style.padding = "10px"; // Adjust padding as needed
+    link.addEventListener("click", () => loadArtifact(artifact.id));
+
+    // Create the heading element
+    const heading = document.createElement("h1");
+    heading.className = "rl-heading-style-h1-3";
+    heading.textContent = artifact.title;
+
+    // Append the heading to the link
+    link.appendChild(heading);
+
+    // Create a wrapper div for styling
+    const divWrapper = document.createElement("div");
+    divWrapper.className = "rl-header44_component rl-padding-section-large";
+    divWrapper.appendChild(link);
+
+    // Append the wrapper div to the container
+    container.appendChild(divWrapper);
+  });
+}
+
+function populateArtifactReflections() {
+  const container = document.getElementById("artifactReflectionsContainer");
+  container.innerHTML = ""; // Clear existing content
+
+  artifacts.forEach((artifact, index) => {
+    if (artifact.reflections && artifact.reflections.length > 0) {
+      // Create a div to hold the artifact link and reflections
+      const artifactBlock = document.createElement("div");
+      artifactBlock.className = "artifact-block";
+
+      // Create a link to the artifact
+      const artifactLink = document.createElement("a");
+      artifactLink.href = `artifact.html?artifactId=${artifact.id}`;
+      artifactLink.textContent = `Artifact ${index + 1}: ${artifact.title}`;
+      artifactLink.className = "artifact-link";
+      artifactBlock.appendChild(artifactLink);
+
+      // Add reflections below the link
+      artifact.reflections.forEach((reflection) => {
+        const reflectionText = document.createElement("p");
+        reflectionText.textContent = `Reflection: ${reflection}`;
+        artifactBlock.appendChild(reflectionText);
+      });
+
+      // Append the entire block to container
+      container.appendChild(artifactBlock);
+    }
+  });
+}
+
+
+function generateReflectionsPDF() {
+  const doc = new jsPDF();
+
+  let yPos = 10; // Initial Y position in the PDF
+  artifacts.forEach((artifact, index) => {
+    if (artifact.reflections && artifact.reflections.length > 0) {
+      // Add the artifact title and URL as text
+      doc.text(10, yPos, `Artifact ${index + 1}: ${artifact.title} (URL: artifact.html?artifactId=${artifact.id})`);
+      yPos += 10;
+
+      // Add reflections
+      artifact.reflections.forEach(reflection => {
+        doc.text(15, yPos, reflection);
+        yPos += 10;
+      });
+
+      yPos += 10; // Add extra space before the next artifact
+    }
+  });
+
+  doc.save("Reflections.pdf");
+}
+
+function resetExperience() {
+  visitedArtifacts.clear();
+  localStorage.removeItem("artifactsData");
+  window.location.href = "artifact.html"; // Redirect to the starting page
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  loadArtifactsAndReflections();
+  const urlParams = new URLSearchParams(window.location.search);
+  console.log(urlParams);
+  const artifactId = urlParams.get("artifactId");
+  const artifactViewer = document.getElementById("artifactViewer");
+  if (artifactId) {
+    // If there is an artifactId in the URL, load that specific artifact
+    console.log(artifactId);
+    await loadArtifact(artifactId);
+  } else if (!artifactId && artifactViewer) {
+    console.log("artifact page detected, loading random artifact");
+    loadRandomArtifact();
+  }
+  if (window.location.pathname.includes("all-artifacts.html")) {
+    console.log("all artifacts page loaded");
+    populateArtifactLinks();
+  }
+  if (window.location.pathname.includes("summary-page.html")) {
+    console.log("summary page loaded");
+    populateArtifactReflections();
+  }
+  const resetButton = document.getElementById("resetButton");
+  if (resetButton) {
+    console.log("reset button present");
+    resetButton.addEventListener("click", resetExperience);
+  }
+  const backButton = document.getElementById("backButton");
+  if (backButton) {
+    backButton.addEventListener("click", () => {
+      window.history.back();
+    });
+  }
+  const downloadReflectionButton = document.getElementById(
+    "downloadReflectionButton"
+  );
+  if (downloadReflectionButton) {
+    downloadReflectionButton.addEventListener("click", generateReflectionsPDF);
+  }
+});
